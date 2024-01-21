aa4433606fb0067293f287ba9f588ca8bb0383f4
s-matsubara <shohs12171217@gmail.com>
2024-01-21 16:19:39 +0900

    セクション4まで終了

--- vanillajs/js/index.js
+++ vanillajs/js/index.js
@@ -216,9 +216,76 @@
 // })
 // console.log(newNumArr);
 
-const nameArr = ["松尾", "三好", "松原"];
+// const nameArr = ["松尾", "三好", "松原"];
 
 // 従来のループ処理
-for (let index = 0; index < nameArr.length; index++) {
-  console.log(``);
-}
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${nameArr[index]}です`));
+
+
+// 松原以外の名前には「さん」をつけて表示する
+// const newArr = nameArr.map((name) => {
+//   if (name !== "松原") {
+//     return `${name}さん`;
+//   } else {
+//     return name;
+//   }
+// })
+// console.log(newArr);
+
+
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 > 2 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// const num = 1300;
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+// }
+// console.log(checkSum(40,500));
+
+
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// truthy, falsyについて
+// const val = "";
+// if (val) {
+//   console.log('truthy');
+// } else {
+//   console.log('falsy');
+// }
+// const isTruthy = val ? "truthy" : "falsy";
+// console.log(isTruthy);
+
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+// ||は左側がtruthyの時、その時点で返却する
+// const num = 'aaa';
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// &&は左側がfalsyの時、その時点で返却する
+// const num2 = "aaa";
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
